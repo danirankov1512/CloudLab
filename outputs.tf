cc1c566d6f83ce7955b64d42d2d7c4e35d11d320
danirankov1512 <dani@rankov.site>
2026-09-02 17:33:33 +0300

    lock: completed docker installation and ssh hardening

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,16 @@ output "vnet_id" {
   value       = azurerm_virtual_network.vnet_tf.id
   description = "Уникалното ID на виртуалната мрежа"
 }
+
+output "vm_public_ip" {
+  value       = azurerm_linux_virtual_machine.web01_tf.public_ip_address
+  description = "Публичният IP адрес на новата Ubuntu виртуалка"
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/ansible/inventory.ini"
+  content  = <<EOT
+[webservers]
+web01_tf ansible_host=${azurerm_linux_virtual_machine.web01_tf.public_ip_address} ansible_user=azureuser ansible_ssh_private_key_file=~/.ssh/id_rsa_azure
+EOT
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
 resource_group_name = "rg-homelab-tf"
 location            = "polandcentral"
 vnet_name           = "vnet2-homelab-tf"
+my_public_ip = "149.62.208.228"
